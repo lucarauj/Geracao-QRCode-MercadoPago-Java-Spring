89b5ee29c056acc4c0c280bf10ca59650a10fc28
lucarauj <lukas.martins193@gmail.com>
2023-11-23 19:34:30 -0300

    Add PixBO

---
+++ pix-api/src/main/java/com/pixapi/domain/entities/PixBO.java
@@ -0,0 +1,54 @@
+package com.pixapi.domain.entities;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class PixBO {
+
+    private UUID id;
+    private BigDecimal amount;
+    private String email;
+    private String description;
+    private String emv;
+    private String base64;
+    private LocalDateTime expiration;
+
+    public PixBO(UUID id, BigDecimal amount, String email, String description, String emv, String base64, LocalDateTime expiration) {
+        this.id = id;
+        this.amount = amount;
+        this.email = email;
+        this.description = description;
+        this.emv = emv;
+        this.base64 = base64;
+        this.expiration = expiration;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getEmv() {
+        return emv;
+    }
+
+    public String getBase64() {
+        return base64;
+    }
+
+    public LocalDateTime getExpiration() {
+        return expiration;
+    }
+}
