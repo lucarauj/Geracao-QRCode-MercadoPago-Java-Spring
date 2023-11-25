d3a06e7f458a199be752e11bbcef89fa1ca793cb
lucarauj <lukas.martins193@gmail.com>
2023-11-25 18:08:13 -0300

    Update RestClientMp para tentar solucionar status code 400

--- pix-api/src/main/java/com/pixapi/infra/mercadoPago/repository/RestClientMpImpl.java
+++ pix-api/src/main/java/com/pixapi/infra/mercadoPago/repository/RestClientMpImpl.java
@@ -3,12 +3,9 @@ package com.pixapi.infra.mercadoPago.repository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pixapi.infra.mercadoPago.dto.MpPixDto;
 import com.pixapi.infra.mercadoPago.dto.MpPixResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -28,8 +25,6 @@ public class RestClientMpImpl implements RestClientMp {
         this.objectMapper = objectMapper;
     }
 
-    private static final Logger logger = LoggerFactory.getLogger(RestClientMpImpl.class);
-
     @Override
     public MpPixResponse createPix(String token, String idempotency, MpPixDto payload) {
         String url = baseUri + "/v1/payments";
@@ -44,41 +39,16 @@ public class RestClientMpImpl implements RestClientMp {
         try {
             HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
-            logRequestAndResponse(url, request, response);
-
             if (response.statusCode() == 200) {
                 MpPixResponse mpPixResponse = objectMapper.readValue(response.body(), MpPixResponse.class);
-                if (mpPixResponse != null) {
-                    return mpPixResponse;
-                } else {
-                    logger.warn("Response body is empty");
-                }
+                return mpPixResponse;
             } else {
-                handleErrorResponse(response);
+                System.out.println("Erro na solicitação: " + response.statusCode());
+                return null;
             }
-        } catch (IOException | InterruptedException e) {
-            handleRequestException(e);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
-
-        return null;
-    }
-
-    private void logRequestAndResponse(String url, HttpRequest request, HttpResponse<String> response) {
-        logger.info("HTTP Request to {}: {}", url, request.method());
-        logger.info("Headers: {}", request.headers().map());
-        logger.info("Request Body: {}", request.bodyPublisher().orElse(HttpRequest.BodyPublishers.noBody()));
-
-        logger.info("HTTP Response: {}", response.statusCode());
-        logger.info("Response Headers: {}", response.headers().map());
-        logger.info("Response Body: {}", response.body());
-    }
-
-    private void handleErrorResponse(HttpResponse<String> response) {
-        logger.warn("HTTP Request returned unexpected status code: {}", response.statusCode());
-        logger.warn("Response Body: {}", response.body());
-    }
-
-    private void handleRequestException(Exception e) {
-        logger.error("Error making HTTP request", e);
     }
 }
